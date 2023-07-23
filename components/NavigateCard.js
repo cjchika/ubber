be41a1a1071d624e4e9f4b911e17f83744c1d9c6
Chijioke Chika <8pluscreative@gmail.com>
2023-07-23 07:00:58 +0100

    Input

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -1,12 +1,31 @@
-import { View, Text } from "react-native";
+import { View, Text, SafeAreaView, StyleSheet } from "react-native";
 import React from "react";
+import tw from "tailwind-react-native-classnames";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { GOOGLE_MAPS_APIKEY } from "@env";
 
 const NavigateCard = () => {
   return (
-    <View>
-      <Text>NavigateCard</Text>
-    </View>
+    <SafeAreaView style={tw`bg-white flex-1 `}>
+      <Text style={tw`text-center py-5 text-xl`}>Good morning, CJ.</Text>
+      <View style={tw`border-t border-gray-200 flex-shrink`}>
+        <View>
+          <GooglePlacesAutocomplete
+            styles={toInputBoxStyles}
+            placeholder="Where to?"
+            debounce={400}
+            nearbyPlacesAPI="GooglePlacesSearch"
+          />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
 export default NavigateCard;
+
+const toInputBoxStyles = StyleSheet.create({
+  container: { backgroundColor: "white", paddingTop: 20, flex: 0 },
+  textInput: { backgroundColor: "#DDDDDF", borderRadius: 0, fontSize: 18 },
+  textInputContainer: { paddingHorizontal: 20, paddingBottom: 0 },
+});
