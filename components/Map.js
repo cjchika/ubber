85ffd16716a3fc6beb728dbe6b722677c09bce50
Chijioke Chika <8pluscreative@gmail.com>
2023-07-23 09:19:01 +0100

    Input

--- components/Map.js
+++ components/Map.js
@@ -1,17 +1,26 @@
+import React, { useRef, useEffect } from "react";
 import { Text, View } from "react-native";
-import React, { Component } from "react";
 import MapView, { Marker } from "react-native-maps";
 import tw from "tailwind-react-native-classnames";
 import { selectOrigin, selectDestination } from "../slices/navSlice";
 import { useSelector } from "react-redux";
+import MapViewDirections from "react-native-maps-directions";
+import { GOOGLE_MAPS_APIKEY } from "@env";
 
 const Map = () => {
   const origin = useSelector(selectOrigin);
-  // console.log(origin?.location?.lat);
-  // console.log(origin?.location?.lng);
+  const destination = useSelector(selectDestination);
+  const mapRef = useRef();
+
+  useEffect(() => {
+    if (!origin || !destination) return;
+
+    mapRef.current.fitToSuppliedMarkers(["origin", "destination"]);
+  }, [origin, destination]);
 
   return (
     <MapView
+      ref={mapRef}
       style={tw`flex-1`}
       mapType="mutedStandard"
       initialRegion={{
@@ -21,6 +30,15 @@ const Map = () => {
         longitudeDelta: 0.005,
       }}
     >
+      {origin && destination && (
+        <MapViewDirections
+          origin={origin.description}
+          destination={origin.description}
+          apikey={GOOGLE_MAPS_APIKEY}
+          strokeWidth={3}
+          strokeColor="black"
+        />
+      )}
       {origin?.location && (
         <Marker
           coordinate={{
@@ -30,6 +48,18 @@ const Map = () => {
           title="Origin"
           description={origin?.description}
           identifier="origin"
+        />
+      )}
+
+      {destination?.location && (
+        <Marker
+          coordinate={{
+            latitude: origin?.location?.lat,
+            longitude: origin?.location?.lng,
+          }}
+          title="Destination"
+          description={origin?.description}
+          identifier="destination"
         />
       )}
     </MapView>
--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -3,18 +3,41 @@ import React from "react";
 import tw from "tailwind-react-native-classnames";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 import { GOOGLE_MAPS_APIKEY } from "@env";
+import { useDispatch } from "react-redux";
+import { useNavigation } from "@react-navigation/native";
+import { setDestination } from "../slices/navSlice";
 
 const NavigateCard = () => {
+  const dispatch = useDispatch();
+  const navigation = useNavigation();
+
   return (
     <SafeAreaView style={tw`bg-white flex-1 `}>
       <Text style={tw`text-center py-5 text-xl`}>Good morning, CJ.</Text>
       <View style={tw`border-t border-gray-200 flex-shrink`}>
         <View>
           <GooglePlacesAutocomplete
+            minLength={2}
+            returnKeyType={"search"}
             styles={toInputBoxStyles}
             placeholder="Where to?"
             debounce={400}
             nearbyPlacesAPI="GooglePlacesSearch"
+            fetchDetails={true}
+            query={{ key: GOOGLE_MAPS_APIKEY, language: "en" }}
+            enablePoweredByContainer={false}
+            onPress={(data, details = null) => {
+              const { geometry } = details;
+              // console.log(geometry);
+              dispatch(
+                setDestination({
+                  location: geometry.location,
+                  description: data.description,
+                })
+              );
+              navigation.navigate("RideOptionCard");
+              // dispatch(setDestination(null));
+            }}
           />
         </View>
       </View>
@@ -26,6 +49,10 @@ export default NavigateCard;
 
 const toInputBoxStyles = StyleSheet.create({
   container: { backgroundColor: "white", paddingTop: 20, flex: 0 },
-  textInput: { backgroundColor: "#DDDDDF", borderRadius: 0, fontSize: 18 },
+  textInput: {
+    backgroundColor: "#DDDDDF",
+    borderRadius: 16,
+    fontSize: 18,
+  },
   textInputContainer: { paddingHorizontal: 20, paddingBottom: 0 },
 });
